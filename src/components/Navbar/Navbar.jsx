ea0b5dae49a10763dd492d44ae32d1f764c24109
Harsh <harshthakur0047@gmail.com>
2024-07-02 11:27:30 +0530

    update  logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,29 @@
 import './Navbar.css'
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../../assets/assets'
 
 const Navbar=()=>{
+
+  const[menu,setMenu]= useState("home")
+
   return(
 <div className='navbar'>
 <img src={assets.logo} alt='' className='logo'></img>
 <ul className='navbar-menu'>
-  <li>Home</li>
-  <li>Menu</li>
-  <li>Mobile-app</li>
-  <li>Contact us</li>
+  <li onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>home</li>
+  <li onClick={()=>setMenu("menu")} className={menu === "menu"? "active":""}>menu</li>
+  <li onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active":""}>mobile-app</li>
+  <li onClick={()=>setMenu("contact-us")} className={menu === "contact-us"?"active":""}>contact us</li>
 </ul>
 <div className="navbar-right">
   <img src={assets.search_icon} alt=''></img>
   <div className="navbar-search-icon">
   <img src={assets.basket_icon} alt=''></img>
+  
   <div className="dot"></div>
-  <button>Sign in</button>
   </div>
+  <button>Sign in</button>
+ 
 </div>
 </div>
   );
