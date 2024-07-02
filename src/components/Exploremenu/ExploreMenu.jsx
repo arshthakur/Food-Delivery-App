dd63a3f160275aec420bfe23aa916496188560ea
Harsh <harshthakur0047@gmail.com>
2024-07-02 17:24:09 +0530

    create new file menu

---
+++ src/components/Exploremenu/ExploreMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets';
+
+const ExploreMenu = ()=>{
+  return(
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>Discover a diverse selection of delicious dishes, crafted with premium ingredients and culinary expertise. From appetizers to desserts, every bite is designed to elevate your dining experience. Enjoy your culinary journey with us!</p>
+
+    </div>
+  );
+}
+
+export default ExploreMenu;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = ()=>{
     <div className='header'>
       <div className='header-contents'>
         <h2>Order your favourite food here</h2>
-        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary experience. Our mission is to satisfy your cravings and elevate your dinning experience, one delicious meal at a time </p>
+        <p>Discover an exquisite selection of dishes, each crafted with premium ingredients and culinary mastery. Whether you’re craving comfort food or gourmet delights, our mission is to tantalize your taste buds and transform every meal into a memorable experience. Satisfy your cravings and elevate your dining journey with us, one delightful bite at a time.</p>
         <button>view menu</button>
       </div>
 
