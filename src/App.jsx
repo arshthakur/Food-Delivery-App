95a42e5d7c02f648c026bbdd0e9c6d5191ef913c
Harsh Thakur <harshthakur0047@gmail.com>
2024-12-11 14:47:19 +0530

    remove offer

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar/Navbar.jsx';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
+import React, { useState } from "react";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
 // import Offers from './pages/Offers/offers';
-import Footer from './components/Footer/Footer.jsx'
-import AppStore from './components/AppStore/AppStore.jsx';
-import PlaceOrder from './pages/Placeorder/PlaceOrder.jsx';
-import LoginPopup from './components/LoginPopup/LoginPopup.jsx';
+import Footer from "./components/Footer/Footer.jsx";
+import AppStore from "./components/AppStore/AppStore.jsx";
+import PlaceOrder from "./pages/Placeorder/PlaceOrder.jsx";
+import LoginPopup from "./components/LoginPopup/LoginPopup.jsx";
 
-const App =()=>{
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
 
-const[showLogin, setShowLogin] = useState(false)
-
-  return(
+  return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}></LoginPopup>:<></>}
-<Navbar setShowLogin={setShowLogin}></Navbar>
-<div className='app'>
-<Routes>
- < Route path='/' element={<Home/>}/>
- < Route path='/cart' element={<Cart/>}/>
- < Route path='/order' element={<PlaceOrder/>}/>
- {/* < Route path='/offers' element={<Offers/>}/> */}
-</Routes>
-</div>
-<AppStore></AppStore>
-<Footer></Footer>
-</>
+      {showLogin ? (
+        <LoginPopup setShowLogin={setShowLogin}></LoginPopup>
+      ) : (
+        <></>
+      )}
+      <Navbar setShowLogin={setShowLogin}></Navbar>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <AppStore></AppStore>
+      <Footer></Footer>
+    </>
   );
-}
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
